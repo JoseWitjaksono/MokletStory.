f3b34f746669b58e5bfea00733126fd579648d1b
Jose Witjaksono <hpsx80@gmail.com>
2017-03-30 09:59:09 +0700

    Setting Firebase Storage & Firebase Database

--- app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl61921272934/mokletstory/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl61921272934/mokletstory/MainActivity.java
@@ -1,15 +1,22 @@
 package id.sch.smktelkom_mlg.project2.xirpl61921272934.mokletstory;
 
+import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import butterknife.BindViews;
+import butterknife.ButterKnife;
+
+
+
 
 public class MainActivity extends AppCompatActivity {
-
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +25,13 @@ public class MainActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                    changeFragment(0);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                    changeFragment(1);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                    changeFragment(2);
                     return true;
             }
             return false;
@@ -32,12 +39,24 @@ public class MainActivity extends AppCompatActivity {
 
     };
 
+        private void changeFragment(int position) {
+
+            android.support.v4.app.Fragment fragment = null;
+            if(position==0)
+                fragment = new Section1Fragment();
+            else if(position==1)
+                fragment = new Section2Fragment();
+            else if(position==2)
+                fragment = new Section3Fragment();
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.container, fragment)
+                    .commitNow();
+        }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
---
+++ app/src/main/java/id/sch/smktelkom_mlg/project2/xirpl61921272934/mokletstory/Section3Fragment.java
@@ -0,0 +1,29 @@
+package id.sch.smktelkom_mlg.project2.xirpl61921272934.mokletstory;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Section3Fragment extends Fragment {
+
+
+	public Section3Fragment() {
+		// Required empty public constructor
+	}
+
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+	                         Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_section3, container, false);
+	}
+
+}
